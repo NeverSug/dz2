b384b9b6134c8f07e288c82e5a80d0be407e3a98
NeverSug <svetik.minina1@yandex.ru>
2026-04-26 00:35:22 +0300

    Update index.php

--- dz3/index.php
+++ dz3/index.php
@@ -84,7 +84,7 @@ $array = [
                     <div class="row">
                         <?php foreach ($array as $arr): ?>
                             <div class="col-md-6">
-                                <a href="blog-single.html" class="blog-entry element-animate" data-animate-effect="fadeIn">
+                                <a href="" class="blog-entry element-animate" data-animate-effect="fadeIn">
                                     <img src="images/<?= $arr['image'] ?>" alt="Image placeholder">
                                     <div class="blog-content-body">
                                         <div class="post-meta">
